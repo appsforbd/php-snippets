3147ddd2353fd4148819e1c0e6885eef759263ff
mariomelchor <mario@mariomelchor.com>
2015-11-09 08:57:29 -0800

    Update pre-get-posts.php

--- pre-get-posts.php
+++ pre-get-posts.php
@@ -1,8 +1,20 @@
 /**
- * Change Query on Posts
+ * Change Query on Post Type
  */
 function et_post( $query ) {
 	if ( is_post_type_archive( 'clients' ) || is_post_type_archive( 'testimonial' ) ) {
+		// Display all posts
+		$query->set( 'posts_per_page', -1 );
+		return;
+	}
+}
+add_action( 'pre_get_posts', 'et_post', 1 );
+
+/**
+ * Change Query on Posts for Archive page and only on main query
+ */
+function et_post( $query ) {
+	if ( is_archive( 'recipes' ) && $query->is_main_query() ) {
 		// Display all posts
 		$query->set( 'posts_per_page', -1 );
 		return;
