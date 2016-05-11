9d0202621c26f5f5a10c8f57cb3948745ad61082
mariomelchor <mario@mariomelchor.com>
2016-05-11 09:32:14 -0700

    Update post-thumbnail-parent.php

--- post-thumbnail-parent.php
+++ post-thumbnail-parent.php
@@ -1,3 +1,18 @@
+// Post Thumbnail of Parent Or Current Post w/ Default Image
+<?php if ( has_post_thumbnail( $post->post_parent, 'post-thumbnail' ) ) {
+	echo get_the_post_thumbnail( $post->post_parent, 'post-thumbnail' );
+	}
+	else {
+		if ( has_post_thumbnail( $post->ID, 'post-thumbnail' ) ) {
+			the_post_thumbnail('post-thumbnail', array( 'class' => 'media-fluid' ));
+		}
+	else {
+			echo '<img src="'. get_template_directory_uri().'/images/feature/page-feature.jpg" alt="'. get_the_title() .'" class="media-fluid">';
+		}
+	}
+?>
+
+//Get Post Thumbnail URL of Parent Or Current Post w/ Default Image
 <?php
 	if ( has_post_thumbnail( $post->post_parent, 'post-thumbnail' ) ) {
 		$featured_image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->post_parent ), 'post-thumbnail' );
@@ -11,7 +26,6 @@
 		$featured_image_url = get_template_directory_uri().'/images/feature/site-feature.jpg';
 	}
 ?>
-
 <div class="page-feature" style="background-image: url('<?php echo $featured_image_url; ?>')">
 	<div class="container">
 		<div class="feature-intro">
