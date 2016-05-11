a28325ee86fb10125b9ac609d76bb700467c40a2
mariomelchor <mario@mariomelchor.com>
2016-05-11 09:24:44 -0700

    Update post-thumbnail-parent.php

--- post-thumbnail-parent.php
+++ post-thumbnail-parent.php
@@ -1,28 +1,35 @@
-// Post Thumbnail of Parent Or Current Post w/ Default Image
-<?php if ( has_post_thumbnail( $post->post_parent, 'post-thumbnail' ) ) {
-	echo get_the_post_thumbnail( $post->post_parent, 'post-thumbnail' );
-	}
-	else {
-		if ( has_post_thumbnail( $post->ID, 'post-thumbnail' ) ) {
-			the_post_thumbnail('post-thumbnail', array( 'class' => 'media-fluid' ));
-		}
-	else {
-			echo '<img src="'. get_template_directory_uri().'/images/feature/page-feature.jpg" alt="'. get_the_title() .'" class="media-fluid">';
-		}
-	}
+// Post Thumbnail Image to URL
+<?php
+if ( has_post_thumbnail() ) {
+	$feature_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'feature-image' );
+	echo '<img src="' . $feature_image_url[0] . '" class="media-fluid border">';
+}
 ?>
 
-//Get Post Thumbnail URL of Parent Or Current Post w/ Default Image
+// Post Thumbnail Image to URL with anchor
 <?php
-	if ( has_post_thumbnail( $post->post_parent, 'post-thumbnail' ) ) {
-		$featured_image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->post_parent ), 'post-thumbnail' );
-		$featured_image_url = $featured_image_url[0];
-	}
-	elseif ( has_post_thumbnail( $post->ID, 'post-thumbnail' ) ) {
-		$featured_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'post-thumbnail' );
-		$featured_image_url = $featured_image_url[0];
-	}
-	else {
-		$featured_image_url = get_template_directory_uri().'/images/feature/site-feature.jpg';
-	}
+if ( has_post_thumbnail() ) {
+	$large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' );
+	echo '<a href="' . $large_image_url[0] . '" title="' . the_title_attribute( 'echo=0' ) . '">';
+	the_post_thumbnail( 'thumbnail' );
+	echo '</a>';
+}
+?>
+
+// Post Thumbnail Image w/ default image
+<?php if ( has_post_thumbnail()) {
+	the_post_thumbnail('post-thumbnail', array( 'class' => 'media-fluid' ));
+}	else {
+	echo '<img src="'. get_template_directory_uri().'/images/feature/page-feature.jpg" alt="'. get_the_title() .'" class="media-fluid">';
+} ?>
+
+// Custom Field Image ID to URL
+<?php
+$image = get_field('secondary_image');
+$size = 'secondary-image';
+
+if( $image ) {
+	$secondary_image_url = wp_get_attachment_image_src( $image, $size );
+	echo '<img src="' . $secondary_image_url[0] . '" class="media-fluid border">';
+}
 ?>
