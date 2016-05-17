2204360d84fc1b31cd607d563987afee6b25c06a
mariomelchor <mario@mariomelchor.com>
2016-05-17 10:02:29 -0700

    Create textarea-explode.php

---
+++ textarea-explode.php
@@ -0,0 +1,8 @@
+$lines = explode("\n", $instruction_textarea); // or use PHP PHP_EOL constant
+if ( !empty($lines) ) {
+  echo '<ul>';
+  foreach ( $lines as $line ) {
+    echo '<li>'. trim( $line ) .'</li>';
+  }
+  echo '</ul>';
+}
