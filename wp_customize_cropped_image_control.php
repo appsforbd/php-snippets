745ba18820ff661f5bf1f077c54a5de7eb74e439
Mario M <Mario M>
2016-10-13 14:03:55 -0700

    added php bracket

--- wp_customize_cropped_image_control.php
+++ wp_customize_cropped_image_control.php
@@ -1,3 +1,4 @@
+	<?php
 	$wp_customize->add_setting('logo_setting');
 
 	$wp_customize->add_control( new WP_Customize_Cropped_Image_Control( $wp_customize, 'logo_setting', array(
@@ -8,8 +9,9 @@
 			'width'       => 550,
 			'height'      => 100,
 	) ) );
-	
+
 $image =  wp_get_attachment_image_src( absint( get_theme_mod( 'logo_setting' ) ), '', false );
 echo $image[0];
 
 echo wp_get_attachment_image( absint( get_theme_mod( 'logo_setting' ) ), '', false );
+?>
