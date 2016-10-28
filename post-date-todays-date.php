6bd533430055d2822e2aff50554a5fcce185a074
mariomelchor <mario@mariomelchor.com>
2016-10-28 13:00:24 -0700

    Added strtotime to time variable

--- post-date-todays-date.php
+++ post-date-todays-date.php
@@ -2,7 +2,7 @@
   // Get Post Date/Todays Date Show Upcoming Event label
   $todays_date = date(get_option( 'date_format' ));
   $event_date = get_the_date();
-  
-  if ( $event_date >= $todays_date ) {
+
+  if ( strtotime( $event_date ) >= strtotime( $todays_date ) ) {
   	echo '<span class="events-upcoming">Upcoming Event</span>';
 } ?>
