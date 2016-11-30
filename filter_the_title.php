f4b76155317b6e3fab743212f452a1156499503e
mariomelchor <mario@mariomelchor.com>
2016-11-30 11:33:11 -0800

    Added example for filtering the_title

---
+++ filter_the_title.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Displays the title for the homepage
+ */
+function dhali_welcome_title( $title, $id ) {
+	if ( is_front_page() && in_the_loop() ) {
+		$title = 'Welcome to ' . get_bloginfo( 'name' );
+	}
+	return $title;
+}
+add_filter( 'the_title', 'dhali_welcome_title', 10, 2 );
