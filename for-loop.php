73d14c5e11fb5724514405998be61b0d3eb87c6a
mariomelchor <mario@mariomelchor.com>
2018-02-27 14:20:28 -0800

    Updated snippet for loop example

--- for-loop.php
+++ for-loop.php
@@ -3,10 +3,23 @@
  * for loop example
  *
  */
-?>
 
-<?php
 	for ( $x = 0; $x <= 10; $x++ ) {
 		echo "The number is: $x <br>";
 	}
 ?>
+
+<?php
+/*
+ * for loop example with html
+ *
+ */
+
+  for($x = 1; $x <=3; $x++) : ?>
+    <div class="col-sm-4">
+      <div class="blog-item">
+        <div class="blog-item-image"></div><!-- /.blog-item-image -->
+        <div class="blog-item-title">Blog Post Title</div><!-- /.blog-item-title -->
+      </div><!-- /.blog-item -->
+    </div><!-- /.col -->
+<?php endfor; ?>
